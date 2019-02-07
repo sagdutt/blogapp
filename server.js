a66f0e0db8e2e5a964a2c84d0bdf2b66ba4e1e9f
sagnikdutta <sagnik.d@cleartrip.com>
2019-02-07 14:49:39 +0530

    allowing preflight request through middleware

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
 
